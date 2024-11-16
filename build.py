0a03a58634ff69c07e6a051ea97c27531257e3ee
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-11-17 02:49:15 +0800

    Update build.py

--- build.py
+++ build.py
@@ -40,7 +40,7 @@ def buildMecabxp():
     with open("do.bat", "w") as ff:
          ff.write(
                 rf"""
-cmake -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ./build/
+cmake -G "Visual Studio 16 2019" -A Win32 -T v141_xp -B ./build/
 cmake --build ./build --config Release -j 14
 """
             ) 
